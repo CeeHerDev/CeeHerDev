1853dd766998f8a2abff3b1856ba31e53fa35e69
Cari <99695348+CeeHerDev@users.noreply.github.com>
2022-06-14 13:51:42 -0400

    Time: 122 ms (55.33%), Space: 50.8 MB (57.78%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+
+var containsDuplicate = function(nums) {
+    
+let mySet = new Set(nums);
+
+if( mySet.size === nums.length) {
+      return false; 
+}
+return true;
+
+
+};
+
